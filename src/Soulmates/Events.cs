41b58a26708ac993b616594e31f377c26b58faf2
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2025-10-28 21:28:25 +0100

    Add a cache to batch tiny status changes
    This should stop the mod from sending a bajillion tiny status changes
    per second. Might help with the lag some players were having.
    
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

--- src/Soulmates/Events.cs
+++ src/Soulmates/Events.cs
@@ -6,23 +6,76 @@ using Photon.Realtime;
 
 namespace Soulmates;
 
+
+// We don't want to be sending a gajillion events per second.
+// For example, hunger ticks EVERY TICK. And it's some tiny value like 1e-05.
+// So, batch some of the values that tick a lot (hunger, cold, hot) and only send them once they go beyond a 1% threshold.
+public class EventCache
+{
+    public static EventCache instance = new EventCache();
+
+    private Dictionary<(CharacterAfflictions.STATUSTYPE, SharedDamageKind), float> cache = new Dictionary<(CharacterAfflictions.STATUSTYPE, SharedDamageKind), float>
+    {
+        { (CharacterAfflictions.STATUSTYPE.Hunger, SharedDamageKind.ADD), 0.0f },
+        { (CharacterAfflictions.STATUSTYPE.Hunger, SharedDamageKind.SUBTRACT), 0.0f },
+        { (CharacterAfflictions.STATUSTYPE.Cold, SharedDamageKind.ADD), 0.0f },
+        { (CharacterAfflictions.STATUSTYPE.Cold, SharedDamageKind.SUBTRACT), 0.0f },
+        { (CharacterAfflictions.STATUSTYPE.Hot, SharedDamageKind.ADD), 0.0f },
+        { (CharacterAfflictions.STATUSTYPE.Hot, SharedDamageKind.SUBTRACT), 0.0f },
+    };
+
+    private float staminaCache = 0.0f;
+    public SharedDamage? cacheEvent(SharedDamage e)
+    {
+        var key = (e.type, e.kind);
+        if (!cache.ContainsKey(key))
+        {
+            return e;
+        }
+        cache[key] += e.value;
+        if (cache[key] < 0.01f)
+        {
+            return null;
+        }
+        e.value = cache[key];
+        cache[key] = 0;
+        return e;
+    }
+
+    public SharedExtraStamina? cacheStamina(SharedExtraStamina e)
+    {
+        if (e.diff > 0)
+        {
+            // Gains are okay, they are always large.
+            return e;
+        }
+        staminaCache += e.diff;
+        if (staminaCache > -0.01f)
+        {
+            return null;
+        }
+        e.diff = staminaCache;
+        staminaCache = 0.0f;
+        return e;
+    }
+}
 public static class Events
 {
-    private static void SendEvent(SoulmateEventType eventType, string e, ReceiverGroup who, bool reliable=true)
+    private static void SendEvent(SoulmateEventType eventType, string e, ReceiverGroup who, bool reliable = true)
     {
         object[] content = [(int)eventType, e];
         RaiseEventOptions raiseEventOptions = new() { Receivers = who };
         var r = reliable ? SendOptions.SendReliable : SendOptions.SendUnreliable;
         PhotonNetwork.RaiseEvent(Plugin.SHARED_DAMAGE_EVENT_CODE, content, raiseEventOptions, r);
     }
-    private static void SendEventTo(SoulmateEventType eventType, string e, int[] targets, bool reliable=true)
+    private static void SendEventTo(SoulmateEventType eventType, string e, int[] targets, bool reliable = true)
     {
         object[] content = [(int)eventType, e];
         RaiseEventOptions raiseEventOptions = new() { TargetActors = targets };
         var r = reliable ? SendOptions.SendReliable : SendOptions.SendUnreliable;
         PhotonNetwork.RaiseEvent(Plugin.SHARED_DAMAGE_EVENT_CODE, content, raiseEventOptions, r);
     }
-    private static void SendToSoulmates(SoulmateEventType eventType, string e, bool reliable=true)
+    private static void SendToSoulmates(SoulmateEventType eventType, string e, bool reliable = true)
     {
         if (Soulmates.NoSoulmates()) return;
 
@@ -40,6 +93,10 @@ public static class Events
             Plugin.Log.LogInfo("$Tried to send a non-shared or absolute status type {statusType}");
             return;
         }
+
+        var e2 = EventCache.instance.cacheEvent(e);
+        if (!e2.HasValue) return;
+        e = e2.Value;
 
         bool reliable = true;
         if (e.kind != SharedDamageKind.SET && e.value < 0.01)
@@ -60,6 +117,9 @@ public static class Events
     }
     public static void SendSharedExtraStaminaEvent(SharedExtraStamina e)
     {
+        var e2 = EventCache.instance.cacheStamina(e);
+        if (!e2.HasValue) return;
+        e = e2.Value;
         SendToSoulmates(SoulmateEventType.SHARED_EXTRA_STAMINA, e.Serialize());
     }
     public static void SendSharedAfflictionEvent(SharedAffliction e)
